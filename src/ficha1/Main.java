14b26539b02ad3fff382c6f2befae732f07ec57e
bormanico <97111633+bormanico@users.noreply.github.com>
2022-01-17 15:33:28 +0000

    Update Main.java
    Adicionei dois println

--- src/ficha1/Main.java
+++ src/ficha1/Main.java
@@ -3,6 +3,9 @@ package ficha1;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!!!");
+
+		System.out.println(soma(70, 9));
+		System.out.println(subtracao(78, 30));
 	}
 
 	public static int soma(int int1, int int2) {
